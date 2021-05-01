7847dee5912c32f41af4fdddca0698a782527abf
Jakub <jakubkrys@gmail.com>
2021-05-01 16:41:03 +0200

    #1 statki, pola, enum

---
+++ src/pl/jakubkrys/battleship_fields/BattleshipMain.java
@@ -0,0 +1,9 @@
+package pl.jakubkrys.battleship_fields;
+
+public class BattleshipMain {
+    public static void main(String[] args) {
+
+        Field f1 = new Field (0,0,Status.EMPTY);
+        f1.setStatus(Status.HIT_EMPTY);
+    }
+}
