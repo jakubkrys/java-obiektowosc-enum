ea4f98365296d87c76a5810d4d8b8b2bb6109620
Jakub <jakubkrys@gmail.com>
2021-05-01 17:20:34 +0200

    #5 klasa main z menu aplikacji konsolowej, enum z wartościami

---
+++ src/pl/jakubkrys/console_app_menu/MenuMain.java
@@ -0,0 +1,16 @@
+package pl.jakubkrys.console_app_menu;
+
+public class MenuMain {
+    public static void main(String[] args) {
+
+        Option o1 = Option.ADD;
+        Option o2 = Option.GET_ALL;
+        Option o3 = Option.REMOVE;
+
+        System.out.println("----- App Menu -----");
+        System.out.println(o1.getOptionValue()+" - "+o1.name().toLowerCase());
+        System.out.println(o2.getOptionValue()+" - "+o2.name().toLowerCase().replaceAll("_","").replace('a','A'));
+        System.out.println(o3.getOptionValue()+" - "+o3.name().toLowerCase());
+
+    }
+}
